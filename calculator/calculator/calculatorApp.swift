3eb622f6e79518aff36dbebabfbc7d908be9ee65
Flash541 <108835853+Flash541@users.noreply.github.com>
2022-07-08 17:40:58 +0200

    Add files via upload

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Botir Jurabaev on 08/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
